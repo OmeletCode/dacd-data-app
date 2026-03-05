3678652f936af23124d8179b07ae7b9eda0c8f9d
yonillo <yonillo43@gmail.com>
2026-03-05 16:33:59 +0000

    feat: conexión con spacex api usando OkHttp

--- spacex-extractor/src/main/java/org/ulpgc/dacd/Main.java
+++ spacex-extractor/src/main/java/org/ulpgc/dacd/Main.java
@@ -1,17 +1,33 @@
-package org.ulpgc.dacd;
+package org.ulpgc.dacd.spacex;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        OkHttpClient client = new OkHttpClient();
+        String url = "https://api.spacexdata.com/v4/starlink";
+
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        System.out.println("Conectando con SpaceX...");
+
+        try (Response response = client.newCall(request).execute()) {
+            if (response.isSuccessful() && response.body() != null) {
+                System.out.println("¡Conexión exitosa! Código: " + response.code());
+                String jsonResponse = response.body().string();
+                System.out.println("Primeros datos recibidos:\n" +
+                        jsonResponse.substring(0, Math.min(jsonResponse.length(), 500)) + "...\n[CONTINÚA]");
+            } else {
+                System.out.println("Error al conectar. Código HTTP: " + response.code());
+            }
+        } catch (IOException e) {
+            System.out.println("Hubo un fallo en la red: " + e.getMessage());
         }
     }
 }
