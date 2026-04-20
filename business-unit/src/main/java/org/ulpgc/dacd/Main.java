83611d9d4a1cb565a48219d8b0e90472d2e74b77
yonillo <proyecto.platanoloco@gmail.com>
2026-04-20 20:03:40 +0100

    feat: implementado lector de eventos historicos NDJSON

--- business-unit/src/main/java/org/ulpgc/dacd/Main.java
+++ business-unit/src/main/java/org/ulpgc/dacd/Main.java
@@ -1,22 +1,38 @@
 package org.ulpgc.dacd;
 
 import io.javalin.Javalin;
+import org.ulpgc.dacd.model.WeatherEvent;
+import org.ulpgc.dacd.reader.EventStoreReader;
+
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("--- Iniciando Business Unit (Datamart & API) ---");
 
-        // 1. Levantamos el servidor web en el puerto 8080
+        // --- PRUEBA DEL LECTOR HISTÓRICO ---
+        EventStoreReader reader = new EventStoreReader();
+
+        // IMPORTANTE: Cambia esta ruta a donde esté realmente tu archivo .events
+        // Puedes hacer clic derecho en tu archivo .events -> Copy Path/Reference -> Absolute Path
+        String rutaClima = "C:/Users/elyon/Desktop/uni/segundo curso/segundo cuatri/DACD/dacd-data-app/eventstore/prediction.Weather/Weather-Feeder/20260420.events";
+
+        List<WeatherEvent> climaHistorico = reader.readWeatherEvents(rutaClima);
+
+        System.out.println("✅ Archivo leído correctamente. Eventos cargados: " + climaHistorico.size());
+        if (!climaHistorico.isEmpty()) {
+            System.out.println("🔍 Ejemplo del primer evento transformado a Java:");
+            System.out.println("   📍 Isla: " + climaHistorico.get(0).location());
+            System.out.println("   🌡️ Temp: " + climaHistorico.get(0).temperature() + " ºC");
+            System.out.println("   ☁️ Clima: " + climaHistorico.get(0).description());
+        }
+        System.out.println("--------------------------------------------------");
+
+        // --- LEVANTAR EL SERVIDOR JAVALIN ---
         Javalin app = Javalin.create().start(8080);
 
-        // 2. Creamos la ruta principal (Home)
         app.get("/", ctx -> {
             ctx.result("🚀 ¡API del Monitor Predictivo de Rain Fade funcionando correctamente!");
-        });
-
-        // 3. Dejamos preparada la ruta que usaremos en el futuro para devolver los datos
-        app.get("/api/alertas", ctx -> {
-            ctx.result("Aquí devolveremos un JSON con los satélites que van a sufrir cortes por el clima...");
         });
     }
 }
